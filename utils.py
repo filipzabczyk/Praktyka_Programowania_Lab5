c038d198ca6d6a40ee692532d5af224403a16441
filipzabczyk <filip.zabczyk@gmail.com>
2026-05-13 14:13:15 +0200

    Fix pylint errors

--- utils.py
+++ utils.py
@@ -1,11 +1,21 @@
-def add (a: int , b: int) -> int :
+"""Module with simple calculator functions."""
+
+
+def add(a: int, b: int) -> int:
+    """Return sum of two numbers."""
     return a + b
 
-def subtract (a: int , b: int) -> int :
+
+def subtract(a: int, b: int) -> int:
+    """Return difference of two numbers."""
     return a - b
 
-def multiply (a: int , b: int) -> int :
+
+def multiply(a: int, b: int) -> int:
+    """Return product of two numbers."""
     return a * b
 
-def divide ( a: int , b : int ) -> float :
+
+def divide(a: int, b: int) -> float:
+    """Return quotient of two numbers."""
     return a / b
